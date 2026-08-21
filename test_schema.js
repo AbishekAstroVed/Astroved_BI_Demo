494790978358ab6611ce5e267cacaf3e1d7d3822
AbishekAstroVed <abishek@astroved.com>
2026-08-21 16:56:48 +0530

    'delete'

--- test_schema.js
+++
@@ -1,9 +0,0 @@
-import { connectMSSQL } from './backend/config/mssql.js';
-
-async function test() {
-  const pool = await connectMSSQL();
-  const result = await pool.request().query("SELECT TOP 1 * FROM Vaaak.ProductAdditionalInfo");
-  console.log(result.recordset[0]);
-  process.exit(0);
-}
-test();
